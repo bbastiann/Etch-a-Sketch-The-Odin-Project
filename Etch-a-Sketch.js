c44b223d77f589e9a4de1e3a121a0617d60dca6b
bbastiann <38893856+bbastiann@users.noreply.github.com>
2025-05-05 21:58:34 -0400

    Create Grid with JavaScript

---
+++ Etch-a-Sketch.js
@@ -0,0 +1,27 @@
+
+
+let flexContainer = document.createElement("div");
+flexContainer.setAttribute("class", "flex-container");
+
+//var square = document.createElement("div");
+//flexContainer.appendChild(square);
+
+document.body.appendChild(flexContainer);
+
+         
+console.log(createSquares());
+
+function createSquares(){
+    for(i = 0; i < 16; i++){
+        let squareContainer = document.createElement("div");
+        squareContainer.setAttribute("class", "square-container");
+
+        for(j = 0; j < 16; j++){
+            let square = document.createElement("div");
+            square.setAttribute("class","square");
+            squareContainer.appendChild(square);
+        }
+        flexContainer.appendChild(squareContainer);
+    }
+    
+}
