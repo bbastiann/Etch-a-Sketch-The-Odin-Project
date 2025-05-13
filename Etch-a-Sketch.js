1fee49bd423f0309ffb71f999faaf34157c9c626
bbastiann <38893856+bbastiann@users.noreply.github.com>
2025-05-12 22:58:04 -0400

    added Game Button and refactor code

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,18 +1,34 @@
+let gameDiv = document.createElement("div");
+gameDiv.setAttribute("class","flex-gameDiv");
 
+let gameButton = document.createElement("button");
+gameButton.setAttribute("class","game-button");
+gameButton.textContent = "Start Game";
+gameButton.addEventListener("click", () => {
+    let number = prompt("what's grid size ?");
+
+    if (number === null) {
+        log.innerText = "OK, maybe next time.";
+    }else{
+        createSquares(number);
+    }
+      
+})
 
 let flexContainer = document.createElement("div");
 flexContainer.setAttribute("class", "flex-container");
 
-document.body.appendChild(flexContainer);
-         
-console.log(createSquares());
+gameDiv.appendChild(gameButton);
+gameDiv.appendChild(flexContainer);
 
-function createSquares(){
-    for(i = 0; i < 16; i++){
+document.body.appendChild(gameDiv);
+
+function createSquares(size){
+    for(i = 0; i < size; i++){
         let squareContainer = document.createElement("div");
         squareContainer.setAttribute("class", "square-container");
 
-        for(j = 0; j < 16; j++){
+        for(j = 0; j < size; j++){
             let square = document.createElement("div");
             square.setAttribute("class","square");
             square.addEventListener("mouseover",changeColor)
