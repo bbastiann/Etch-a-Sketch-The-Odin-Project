8e13872461c530de425d6e7d9c3bb95ca4723aac
bbastiann <38893856+bbastiann@users.noreply.github.com>
2025-05-14 01:32:03 -0400

    Restart Game when press Button

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,18 +1,18 @@
 let gameDiv = document.createElement("div");
-gameDiv.setAttribute("class","flex-gameDiv");
+gameDiv.setAttribute("class", "flex-gameDiv");
 
 let gameButton = document.createElement("button");
-gameButton.setAttribute("class","game-button");
+gameButton.setAttribute("class", "game-button");
 gameButton.textContent = "Start Game";
 gameButton.addEventListener("click", () => {
     let number = prompt("what's grid size ?");
 
     if (number === null) {
         log.innerText = "OK, maybe next time.";
-    }else{
+    } else {
         createSquares(number);
     }
-      
+
 })
 
 let flexContainer = document.createElement("div");
@@ -23,22 +23,28 @@ gameDiv.appendChild(flexContainer);
 
 document.body.appendChild(gameDiv);
 
-function createSquares(size){
-    for(i = 0; i < size; i++){
+function createSquares(size) {
+
+    let existingContainer = document.querySelectorAll(".square-container");
+    existingContainer.forEach(e => e.remove());
+
+
+    for (let i = 0; i < size; i++) {
         let squareContainer = document.createElement("div");
         squareContainer.setAttribute("class", "square-container");
 
-        for(j = 0; j < size; j++){
+        for (let j = 0; j < size; j++) {
             let square = document.createElement("div");
-            square.setAttribute("class","square");
-            square.addEventListener("mouseover",changeColor)
+            square.setAttribute("class", "square");
+            square.addEventListener("mouseover", changeColor)
             squareContainer.appendChild(square);
         }
         flexContainer.appendChild(squareContainer);
     }
-    
+
+
 }
 
-function changeColor(){
-    this.style.backgroundColor = "black";  
+function changeColor() {
+    this.style.backgroundColor = "black";
 }
